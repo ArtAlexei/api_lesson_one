9eb3d6eeb3703e12a66d2f8519a45365e4b2fe67
alexei <artalexeimail@gmail.com>
2021-10-28 20:55:40 +0300

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    locations = ['london', 'svo', 'Череповец']
+    for location in locations:
+        url = f'https://wttr.in/{location}?nTqm&lang=ru'
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
