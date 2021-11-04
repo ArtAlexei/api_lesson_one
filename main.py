1712074537e242803142fba28b2a24c3e0250ebb
alexei <artalexeimail@gmail.com>
2021-11-04 22:17:42 +0300

    add params to request, fix bugs

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import requests
 def main():
     locations = ['london', 'svo', 'Череповец']
     for location in locations:
-        url = f'https://wttr.in/{location}?nTqm&lang=ru'
-        response = requests.get(url)
+        param = {'n': '', 'T': '', 'q': '', 'm': '', 'lang': 'ru'}
+        url = f'https://wttr.in/{location}'
+        response = requests.get(url, param)
         response.raise_for_status()
         print(response.text)
-
 
 if __name__ == '__main__':
     main()
